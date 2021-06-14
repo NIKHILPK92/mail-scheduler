533b92f7e9d9eb42b36659866a8bfcc771642a66
Nikhil P K <33482945+NIKHILPK92@users.noreply.github.com>
2021-06-14 10:01:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const cron =require("node-cron");
 
 
 let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    service: 'gmail',
+    host: process.env.SMTP,
+    port: process.env.PORT,
+    service: process.env.SERVICE,
     secure: true,
     auth: {
-        user: "2025slack@gmail.com",
-        pass: "Power@321"
+        user: process.env.USER,
+        pass: process.env.PASSWORD
     }
 })
 
 cron.schedule('* * * * *', () =>{
 let mailOptions = '';
-let userData = ["nikhilpktcr@gmail.com", "nikhilpktcr@outlook.com"] //we can use userTable or collection to send data by looping it
+let userData = process.env.USER_DETAILS //we can use userTable or collection to send data by looping it
     for(let i = 0; i< userData.length; i++){
         mailOptions = {
-            from: "2025slack@gmail.com",
+            from: process.env.USER_MAIL
             to: userData[i],
             subject: "Daily schedule",
             text: "sheduled task"
@@ -33,14 +33,3 @@ let userData = ["nikhilpktcr@gmail.com", "nikhilpktcr@outlook.com"] //we can use
         }); 
     }
 })
-
-
-
-
-
-
-
-
-
-
-
